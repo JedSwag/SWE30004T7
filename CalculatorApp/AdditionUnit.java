f922a23cdf0f7bc2cc4ed89233f1e37627099b51
JedSwag <jedpsharman@gmail.com>
2019-10-17 20:47:49 +1100

    Added Calculation Unit

---
+++ CalculatorApp/AdditionUnit.java
@@ -0,0 +1,11 @@
+
+public class AdditionUnit extends CalculationUnit
+{
+    @Override
+    public double Calculate(double inputA, double inputB)
+    {
+        double result = inputA + inputB;
+        
+        return result;
+    }
+}
--- CalculatorApp/CalculationUnit.java
+++ CalculatorApp/CalculationUnit.java
@@ -2,6 +2,11 @@ public class CalculationUnit
 {
     public CalculationUnit()
     {
-        
+        //place holder
+    }
+    
+    public double Calculate(double inputA, double inputB)
+    {
+        return 0.0;
     }
 }
